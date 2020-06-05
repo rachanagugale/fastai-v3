88238aa821f38750a20bc3149b2521b7db47fcdb
Rachana Gugale <42697335+rachanagugale@users.noreply.github.com>
2020-06-05 08:26:57 +0530

    Updated server.py to match custom model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-10WDwVb1zW_imFec8__QJPbOXcsDeM_'
 export_file_name = 'export.pkl'
+classes = ['KnifeAttack', 'Shooting']
 
-classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
